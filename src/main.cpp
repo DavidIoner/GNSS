5e3eac61743844c0dea8e1b0b1d84fcfaa1f6dc8
DavidIoner <davidhioner@gmail.com>
2023-09-25 17:36:27 -0300

    serialização JSON

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,25 @@
 #include <TinyGPS++.h>
 #include <SoftwareSerial.h>
+#include <ArduinoJson.h>
 
 
+String JSONData;
 TinyGPSPlus gps;
 SoftwareSerial gpsSerial(16, 17);  // RX, TX - ajuste os pinos conforme necessário
 
 String horaAnterior = "";
+
+String toJSON(float latitude, float longitude, String hora) {
+    StaticJsonDocument<200> doc;
+    doc["latitude"] = latitude;
+    doc["longitude"] = longitude;
+    doc["hora"] = hora;
+
+    // Serializar o JSON em uma string
+    String jsonStr;
+    serializeJson(doc, jsonStr);
+    return jsonStr;
+}
 
 void setup() {
   Serial.begin(9600);
@@ -21,12 +35,15 @@ void loop() {
         float longitude = gps.location.lng();
         String hora = String(gps.time.hour()) + ":" + String(gps.time.minute()) + ":" + String(gps.time.second());
         if (hora != horaAnterior) {
+          Serial.println("--------------------");
           Serial.print("Latitude: ");
           Serial.println(latitude, 5);
           Serial.print("Longitude: ");
           Serial.println(longitude, 5);
           Serial.print("Hora: ");
           Serial.println(hora);
+          JSONData = toJSON(latitude, longitude, hora);
+          Serial.println(JSONData);
         }
 
         horaAnterior = hora;
@@ -34,3 +51,4 @@ void loop() {
     }
   }
 }
+
